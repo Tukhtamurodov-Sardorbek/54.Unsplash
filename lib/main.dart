f4118e2d7a29e5d63db3bd7514cb8cd587e5a976
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-10-07 21:06:50 +0500

    🍀 Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:unsplash/bloc/bloc.dart';
+import 'package:unsplash/bloc/post/bloc.dart';
 import 'package:unsplash/data/local/posts.dart';
 import 'package:unsplash/data/local/hive_database.dart';
 import 'package:unsplash/route/router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unsplash/data/repository/photo_repository.dart';
 
+import 'bloc/bloc_observer.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  // * To determine the sequence of events and states that have occurred
+  Bloc.observer = CustomBlocObserver();
 
   // * Orientation
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -34,12 +38,7 @@ void main() async{
       Hive.registerAdapter(PostsAdapter());
       await LocalDatabase.getInstance();
 
-      runApp(
-        BlocProvider<PostBloc>(
-          create: (context) => PostBloc(PostRepository()),
-          child: const MyApp(),
-        ),
-      );
+      runApp(const MyApp());
     },
     (error, stack) {
       print('#########################################################');
@@ -62,23 +61,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Unsplash Posts',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black,
-      ),
+    return  BlocProvider<PostBloc>(
+        create: (context) => PostBloc(PostRepository()),
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Unsplash Posts',
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            scaffoldBackgroundColor: Colors.black,
+          ),
 
-      routerConfig: AppRouter.appRouter(),
+          routerConfig: AppRouter.appRouter(),
 
-      // * To remove the glow on the whole application
-      builder: (context, child) {
-        return ScrollConfiguration(
-          behavior: NoGlowBehavior(),
-          child: child!,
-        );
-      },
+          // * To remove the glow on the whole application
+          builder: (context, child) {
+            return ScrollConfiguration(
+              behavior: NoGlowBehavior(),
+              child: child!,
+            );
+          },
+        ),
     );
   }
 }
