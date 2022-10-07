48240ac046ae0445185caf3a6bf0311485874e13
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-10-07 17:05:25 +0500

    🍀 Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:unsplash/bloc/bloc.dart';
+import 'package:unsplash/data/local/posts.dart';
+import 'package:unsplash/data/local/hive_database.dart';
 import 'package:unsplash/route/router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unsplash/data/repository/photo_repository.dart';
@@ -27,9 +30,13 @@ void main() async{
 
   runZonedGuarded<Future<void>>(
     () async {
+      await Hive.initFlutter();
+      Hive.registerAdapter(PostsAdapter());
+      await LocalDatabase.getInstance();
+
       runApp(
         BlocProvider<PostBloc>(
-          create: (context) => PostBloc(PhotoRepository()),
+          create: (context) => PostBloc(PostRepository()),
           child: const MyApp(),
         ),
       );
