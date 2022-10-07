36707bb194b503c1d3c631a209a407f47ff57453
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-10-07 17:05:34 +0500

    🍀 Code Update

--- lib/data/repository/photo_repository.dart
+++ lib/data/repository/photo_repository.dart
@@ -1,34 +1,34 @@
 import 'package:dio/dio.dart';
-import 'package:unsplash/data/model/photo.dart';
+import 'package:unsplash/data/model/post.dart';
 import 'package:unsplash/data/provider/api_client.dart';
 import 'package:unsplash/data/provider/response_handler.dart';
 import 'package:unsplash/data/provider/server_error.dart';
 
-class PhotoRepository {
+class PostRepository {
   ApiClient apiClient = ApiClient();
 
-  Future<ResponseHandler<List<Photo>>> _fetchPhotos() async {
-    List<Photo>? photos;
+  Future<ResponseHandler<List<Post>>> _fetchPosts({required int page, int perPage = 30}) async {
+    List<Post>? posts;
 
     String url = 'https://api.unsplash.com/photos';
 
     try {
-      List<dynamic>? posts = await apiClient.GET(api: url, params: ApiClient.paramsGET(page: 1, perPage: 30));
-      if(posts != null){
-        photos = posts.map((post) => Photo.fromJson(post)).toList();
+      List<dynamic>? _posts = await apiClient.GET(api: url, params: ApiClient.paramsGET(page: page, perPage: perPage));
+      if(_posts != null){
+        posts = _posts.map((post) => Post.fromJson(post)).toList();
       }
     } catch (error, stacktrace) {
       print('\t Exception occurred: $error stacktrace: $stacktrace');
       return ResponseHandler()..error = ServerError.withError(error as DioError);
     }
 
-    return ResponseHandler()..data = photos;
+    return ResponseHandler()..data = posts;
   }
 
 
-  Future<dynamic> getPhotos() async {
-    final response = await _fetchPhotos();
-    print('🍀 ****** PHOTO REPOSITORY ******');
+  Future<dynamic> getPosts({required int page, int perPage = 30}) async {
+    final response = await _fetchPosts(page: page, perPage: perPage);
+    print('🍀 ****** POST REPOSITORY ******');
     print('IS RESPONSE NULL: ${response.data == null}');
     print('******************************');
 
