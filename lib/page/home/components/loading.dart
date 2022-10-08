2bdcfad3411063e3f99625db09acf58b967c53a9
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-10-09 00:25:18 +0500

    🍀 Code Update

---
+++ lib/page/home/components/loading.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+ScaffoldFeatureController<SnackBar, SnackBarClosedReason> loading(BuildContext context){
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Colors.black45,
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Theme(
+            data: ThemeData(
+              cupertinoOverrideTheme: const CupertinoThemeData(
+                brightness: Brightness.light,
+              ),
+            ),
+            child: CupertinoActivityIndicator(
+              radius: 12,
+              color: Colors.amber.shade200,
+              animating: true,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text('Loding...', style: TextStyle(color: Colors.amber.shade200),),
+        ],
+      ),
+    ),
+  );
+}
--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unsplash/bloc/post/bloc.dart';
 import 'package:unsplash/page/home/components/grid_view.dart';
 import 'package:unsplash/page/home/components/loader.dart';
+import 'package:unsplash/page/home/components/loading.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,30 +15,7 @@ class HomePage extends StatelessWidget {
       body: BlocConsumer<PostBloc, PostState>(
         listener: (BuildContext context, PostState state) {
           if (state is LoadingMoreState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Colors.black45,
-                    content: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Theme(
-                          data: ThemeData(
-                            cupertinoOverrideTheme: const CupertinoThemeData(
-                              brightness: Brightness.light,
-                            ),
-                          ),
-                          child: CupertinoActivityIndicator(
-                            radius: 12,
-                            color: Colors.amber.shade200,
-                            animating: true,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Text('Loding...', style: TextStyle(color: Colors.amber.shade200),),
-                      ],
-                    ),
-                ),
-            );
+            loading(context);
           }
           return;
         },
